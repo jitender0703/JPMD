7645902b853fafbc84bc0accd253f0f1be696fb5
jitender0703 <jitenderpal0703@gmail.com>
2023-03-12 03:02:58 -0700

    first commit

---
+++ JPMD/src/main/java/com/example/jp/JPMD/JpmdApplication.java
@@ -0,0 +1,13 @@
+package com.example.jp.JPMD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpmdApplication.class, args);
+	}
+
+}
---
+++ JPMD/src/test/java/com/example/jp/JPMD/JpmdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jp.JPMD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
